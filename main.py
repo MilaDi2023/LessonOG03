4815a1cefb763c7dc98c55e976531da60d89e624
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-03-27 16:37:12 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
 while running:
-    pass
+    screen.fill(color) # Заливаем фон игры рандомным цветом
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get.pos() # Проверка координат клика мышки на попадание в диапазон смайла-мишени
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                # Рандомное обновление координат смайлика-мишени
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y)) # Отрисовка на экране смайлика-мишени
+    pygame.display.update()
+
 
 pygame.quit()
