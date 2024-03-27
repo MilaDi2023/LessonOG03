c683343bc006264ea0e095767c141ce92bf35e2b
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-03-27 22:50:32 +0300

    Image change code was added

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Задаем размеры экрана игры
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption("Игра Тир") # Задаем название заголовка окна игры
+pygame.display.set_caption("Игра Тир")
 
-icon = pygame.image.load("Images/Target_icon.png") # Прописываем путь к картинке
-pygame.display.set_icon(icon) # Назначаем картинку в качестве иконки игры
+icon = pygame.image.load("Images/Target_icon.png")
+pygame.display.set_icon(icon)
 
-target_image = pygame.image.load("Images/smaylik.png") # Прописываем путь к картинке мишени
-target_width = 80 # Задаём ширину мишени
-target_height = 80 # Задаём высоту мишени
+# Загружаем два изображения
+target_image = pygame.image.load("Images/smaylik.png")
+target_clicked_image = pygame.image.load("Images/smaylikIn.png")
 
-# Задаём переменные для рандомных координат (х, у) мишени
+target_width = 80
+target_height = 80
+
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-# Задаем переменную рандомного цвета игрового фона
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    screen.fill(color) # Заливаем фон игры рандомным цветом
+    screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos() # Проверка координат клика мышки на попадание в диапазон смайла-мишени
+            mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                # Рандомное обновление координат смайлика-мишени
+                screen.blit(target_clicked_image,
+                            (target_x, target_y))  # Вместо изменения координат, меняем изображение
+                pygame.display.update()  # И обновляем экран
+                time.sleep(0.5)  # Задерживаем на полсекунды
+
+                # и затем обновляем координаты смайлика в случайное место
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-    screen.blit(target_image, (target_x, target_y)) # Отрисовка на экране смайлика-мишени
+    screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
-
 
 pygame.quit()
