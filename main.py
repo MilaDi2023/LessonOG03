f433de6d34cfd0d77a399284ae6ef0964c4f8bf0
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-03-27 07:35:47 +0300

    add new constants

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Задаем размеры экрана игры
+
+pygame.display.set_caption("Игра Тир") # Задаем название заголовка окна игры
+icon = pygame.image.load("")
+
 
 running = True
 while running:
