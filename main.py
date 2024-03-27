c92999bbe55334aa18dc645f3df8d8174f3c5811
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-03-27 07:49:59 +0300

    add icon details comment

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Задаем размеры экрана игры
 
 pygame.display.set_caption("Игра Тир") # Задаем название заголовка окна игры
-icon = pygame.image.load("Images/Target_icon.png")
+icon = pygame.image.load("Images/Target_icon.png") # Прописываем путь к иконке
+
+
 
 
 running = True
