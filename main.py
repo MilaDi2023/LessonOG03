16cb1b75ae2dd6f9d29d212dd491d1cfd40465d6
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-03-28 13:23:29 +0300

    better features were added

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import random
 import time
 
 pygame.init()
-
-# Инициализируем mixer для воспроизведения звуков
 pygame.mixer.init()
 
-# Загружаем звуковой файл
 sound = pygame.mixer.Sound("Sounds/ooops.mp3")
 
 SCREEN_WIDTH = 800
@@ -22,31 +19,77 @@ pygame.display.set_icon(icon)
 target_image = pygame.image.load("Images/smaylik.png")
 target_clicked_image = pygame.image.load("Images/smaylikIn.png")
 
+font = pygame.font.Font(None, 40)
 target_width = 80
 target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-running = True
-while running:
-    screen.fill(color)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+def game_loop():
+    global target_x, target_y
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    start_ticks = pygame.time.get_ticks()
+    user_score = 0
+    smiley_score = 0
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                seconds = (pygame.time.get_ticks() - start_ticks) / 1000
+                if seconds < 30:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                        screen.blit(target_clicked_image, (target_x, target_y))
+                        pygame.display.update()
+                        sound.play()
+                        user_score += 1
+                        time.sleep(1)
+                        target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                    else:
+                        smiley_score += 1
+                        target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        screen.fill(color)
+        seconds = (pygame.time.get_ticks() - start_ticks) / 1000
+        if seconds < 30:
+            screen.blit(target_image, (target_x, target_y))
+        elif seconds >= 30:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                screen.blit(target_clicked_image, (target_x, target_y))
-                pygame.display.update()
-                sound.play()  # Воспроизводим звук при попадании по изображению
-                time.sleep(0.3)
+            button = pygame.image.load("Images/ContinueButton.png")
+            stop_button = pygame.image.load("Images/StopButton.png")
 
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-    screen.blit(target_image, (target_x, target_y))
-    pygame.display.update()
+            buttons_gap = 80
+            button_x = (SCREEN_WIDTH // 2) - button.get_width() - buttons_gap // 2
+            button_y = (SCREEN_HEIGHT // 2) - button.get_height() // 2
 
+            stop_button_x = (SCREEN_WIDTH // 2) + buttons_gap // 2
+            stop_button_y = button_y
+
+            screen.blit(button, (button_x, button_y))
+            screen.blit(stop_button, (stop_button_x, stop_button_y))
+        score_text = font.render("Юзер - Смайлик {0}:{1}".format(user_score, smiley_score), True, (0, 0, 0))
+        screen.blit(score_text, ((SCREEN_WIDTH - score_text.get_rect().width) // 2, 10))
+        pygame.display.flip()
+
+        while not running:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    if (button_x < mouse_x < button_x + button.get_width()) and (
+                            button_y < mouse_y < button_y + button.get_height()):
+                        game_loop()
+                    elif (stop_button_x < mouse_x < stop_button_x + stop_button.get_width()) and (
+                            stop_button_y < mouse_y < stop_button_y + stop_button.get_height()):
+                        pygame.quit()
+                        quit()
+
+
+game_loop()
 pygame.quit()
